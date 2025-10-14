5e7fa173820679b6965e3a39b76d5aac79ded9d3
vuttgl123 <phamtuanvu1401@gmail.com>
2025-10-14 23:25:40 +0700

    Initial commit

---
+++ src/main/java/example/backend_mini_app/BackendMiniAppApplication.java
@@ -0,0 +1,13 @@
+package example.backend_mini_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMiniAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMiniAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/backend_mini_app/BackendMiniAppApplicationTests.java
@@ -0,0 +1,13 @@
+package example.backend_mini_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMiniAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
